0013d5c18e7ae51dbf415d176c01344852879e65
EmiGarBar <110506022+EmiGarBar@users.noreply.github.com>
2023-06-04 10:10:20 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup ()  
+{
+video=createCapture (VIDEO);
+video.size(550,500);
+
+canvas=createCanvas(550,500);
+canvas.position(560, 150);
+
+poseNet=ml5.poseNet(video,modeLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modeLoaded ()
+{
+    console.log('posesNet esta inicializado');
+}
+
+function gotPoses (results) {
+    if(results.length >0)
+{
+    console.log(results);
+}
+}
+
